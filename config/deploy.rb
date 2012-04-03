539680d031409d4f1537a1e4f700876cee83c3e3
Thomas Wiese <thomas@madewithlove.be>
2012-04-03 10:30:00 +0200

    changed the deploy.rb file for testing locally

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,9 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "webco-basic"
+set :repository, "git@github.com:playfield-media/webco-basic.git"
+set :scm, :git
+set :deploy_to, "/Users/Wiese/Desktop/test"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :deploy_via, :remote_cache
+set :copy_exclude, [".git", ".DS_Store", ".gitignore", ".gitmodules"]
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+server "example.org", :app
